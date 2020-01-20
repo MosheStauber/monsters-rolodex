7de192276a378740806304d66b3a943eb3ee1ab1
Moshe <moshe.stauber89@gmail.com>
2020-01-19 19:21:36 -0500

    Updates

---
+++ precache-manifest.08cc79cf226dbd4f03e51be45be65b58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283528c3ca7c8c471e57136870244757",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8b9d517969693327ae2c",
+    "url": "/monsters-rolodex/static/css/main.dd5b120b.chunk.css"
+  },
+  {
+    "revision": "1093a6240466003a5984",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b9d517969693327ae2c",
+    "url": "/monsters-rolodex/static/js/main.62e28d59.chunk.js"
+  },
+  {
+    "revision": "fcc6c7087f08bfeac99a",
+    "url": "/monsters-rolodex/static/js/runtime-main.52149680.js"
+  }
+]);
